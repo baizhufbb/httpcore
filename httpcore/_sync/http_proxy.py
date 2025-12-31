00b33c3aedb78132bcac8935af3fd8014ffb5f47
baizhu <806927537@qq.com>
2025-12-31 12:58:01 +0800

    Fix kwargs definition in sync version

--- httpcore/_sync/http_proxy.py
+++ httpcore/_sync/http_proxy.py
@@ -307,6 +307,11 @@ class TunnelHTTPConnection(ConnectionInterface):
                 alpn_protocols = ["http/1.1", "h2"] if self._http2 else ["http/1.1"]
                 ssl_context.set_alpn_protocols(alpn_protocols)
 
+                kwargs = {
+                    "ssl_context": ssl_context,
+                    "server_hostname": self._remote_origin.host.decode("ascii"),
+                    "timeout": timeout,
+                }
                 try:
                     with Trace("start_tls", logger, request, kwargs) as trace:
                         stream = stream.start_tls(**kwargs)
